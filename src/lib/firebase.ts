08c793db0b828639fedbdb1409fa9f6cad589c12
karthik hegde <hegde.accenture@gmail.com>
2025-10-30 17:36:23 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,14 +14,21 @@ const firebaseConfig = {
 };
 
 let app: FirebaseApp;
+let auth: Auth;
+let db: Firestore;
+let storage: FirebaseStorage;
+
 if (typeof window !== 'undefined' && !getApps().length) {
   app = initializeApp(firebaseConfig);
-} else {
+  auth = getAuth(app);
+  db = getFirestore(app);
+  storage = getStorage(app);
+} else if (typeof window !== 'undefined') {
   app = getApp();
+  auth = getAuth(app);
+  db = getFirestore(app);
+  storage = getStorage(app);
 }
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
+// @ts-ignore
 export { app, auth, db, storage };
